c070ddd293242cf00cdab03ab3ec7f8dfb126a2f
Miguel Bautista <bautistaperez3m@gmail.com>
2023-12-17 08:24:39 +0100

    z

---
+++ src/app/core/i18n.service.ts
@@ -0,0 +1,15 @@
+// i18n.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class I18nService {
+  constructor(private http: HttpClient) {}
+
+  getTranslation(lang: string): Observable<any> {
+    return this.http.get(`./assets/i18n/${lang}.json`);
+  }
+}
